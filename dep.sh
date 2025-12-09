03074e129ee2cfeaf5f403efdc5594df703102e6
filippo-ferrando <fferrando@elemento.cloud>
2025-12-09 17:26:42 +0100

    caelestia custom config

--- dep.sh
+++ dep.sh
@@ -8,6 +8,17 @@ paru -S nvim fzf npm
 git clone https://github.com/LazyVim/starter ~/.config/nvim
 rm -rf ~/.config/nvim/.git
 
-
 # additional 
 paru -S hyprmon-bin hyprpolkitagent thunar docker distrobox lazygit lazydocker tumbler vorta tailscale gh-cli telegram-desktop spotify vesktop gvfs gnome-disk-utility wireguard-tools
+
+# dots
+paru -S caelestia-shell
+paru -S hyprpicker wl-clipboard cliphist inotify-tools app2unit wireplumber trash-cli foot fastfetch btop jq eza adw-gtk-theme papirus-icon-theme ttf-jetbrains-mono-nerd
+
+# dots config
+git clone git@github.com:filippo-ferrando/caelestia.git ~/.local/share/caelestia
+cd ~/.config
+ln -s ~/.local/share/caelestia/foot foot
+ln -s ~/.local/share/caelestia/hypr hypr
+ln -s ~/.local/share/caelestia/btop btop
+ln -s ~/.local/share/caelestia/uwsm uwsm 
