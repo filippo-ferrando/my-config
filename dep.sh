c2e92034f3955fcddb1c79f59e2d697557128d5e
filippo-ferrando <fferrando@elemento.cloud>
2025-12-09 17:41:08 +0100

    added lazyssh

--- dep.sh
+++ dep.sh
@@ -10,7 +10,7 @@ git clone https://github.com/LazyVim/starter ~/.config/nvim
 rm -rf ~/.config/nvim/.git
 
 # additional 
-paru -S hyprmon-bin hyprpolkitagent thunar docker distrobox lazygit lazydocker tumbler vorta tailscale gh-cli telegram-desktop spotify vesktop gvfs gnome-disk-utility wireguard-tools
+paru -S hyprmon-bin hyprpolkitagent thunar docker distrobox lazygit lazydocker lazyssh tumbler vorta tailscale gh-cli telegram-desktop spotify vesktop gvfs gnome-disk-utility wireguard-tools
 
 # my-alias
 ./aliases.sh
