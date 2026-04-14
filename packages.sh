10b96bf408657b95c88e1f802e4186eff60e4403
filippo-ferrando <fferrando@elemento.cloud>
2026-04-14 17:50:17 +0200

    update

---
+++ packages.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+paru -Syyuu
+
+paru -S neovim bruno-bin remmina freerdp ansible argocd borg btrfs-assistant grub-btrfs cowsay lolcat cups distrobox docker docker-compose helm hping k9s lazydocker lazygit localsend mosh uv rclone rsync tailscale telegram-desktop vorta wireguard-tools
